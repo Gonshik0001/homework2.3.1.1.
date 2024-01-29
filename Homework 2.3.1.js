597f9ebf5c9d3ab2001ab42b047fc7af99f05e5b
Gonshik0001 <132338931+Gonshik0001@users.noreply.github.com>
2024-01-29 10:49:54 +0400

    Add files via upload

---
+++ Homework 2.3.1.js
@@ -0,0 +1,60 @@
+const {newPlayer= [
+    {
+        "id": 1, "name": "Ivan", "scorePoints": 4500
+    },
+    {
+        "id": 2, "name": "Petr", "scorePoints": 3600
+    },
+    {
+        "id": 3, "name": "Vadim", "scorePoints": 3433
+    },
+    {
+        "id": 4, "name": "Olga", "scorePoints": 2356
+    },
+]
+for (let i = 0; i < newPlayer.scorePoints; i++) {
+    console.log("index is" +i + "and value is")
+  }
+    constructor ("id", "name", "scorePoints"){
+        this.playerId = "id";
+        this.playerName = "name";
+        this.playerScorePoints= "scorePoints";
+thispointsArray=[]
+    add points value{ 
+        calculatescorePointsSum()
+        let sum =0;
+        for (let eachvalue of scorePoints)
+        sum +=eachvalue}
+    return sum;
+    }
+
+    }
+   console.log(newPlayer.scorePointsSum)
+
+//вывод значения
+
+let obj = { a: 1, b: 2, c: 3 }
+
+for(elementKey in obj) {
+console.log(obj[elementKey])
+}
+
+//1
+//2
+//3
+
+
+//вывод ключа
+
+let obj = { a: 1, b: 2, c: 3 }
+
+for(elementKey in obj) {
+console.log(elementKey)
+}
+
+//a
+
+//b
+
+//c
+
